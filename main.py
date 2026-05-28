dd9c657b0ceb53cfbe68be58de36680c4dbe4e11
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-28 12:16:29 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class userEpdateWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = userEpdateWorker()
+    print("Worker engine initialized.")
